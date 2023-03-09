e5b02736bd46b2e48d24591a61a40893e8ee914d
singledog957 <22373062@buaa.edu.cn>
2023-03-09 23:12:00 +0800

    dd

--- Ex.c
+++ Ex.c
@@ -1,9 +1,9 @@
-#define _CRT_SECURE_NO_WARNINGS 
-#include<stdio.h>
-#include<stdlib.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
 int a[3000006];
-int cmp(const void*, const void*);
-int comp(int*);
+int cmp(const void *, const void *);
+int comp(int *);
 int k;
 int main()
 {
@@ -14,13 +14,13 @@ int main()
 		scanf("%d", &a[i]);
 	}
 	/*qsort(a, 3 * k + 1, sizeof(int), cmp);*/
-	printf("%d", comp(a));
+	printf("%d5", comp(a));
 }
-//int cmp(const void* a, const void* b)
+// int cmp(const void* a, const void* b)
 //{
 //	return (*(int*)a - *(int*)b);
-//}
-int comp(int* a)
+// }
+int comp(int *a)
 {
 	int i = 0;
 	for (; i < 3 * k + 1; i += 3)
@@ -29,7 +29,10 @@ int comp(int* a)
 			continue;
 		break;
 	}
-	if (a[i] == a[i + 1]) return a[i + 2];
-	else if (a[i] == a[i + 2]) return a[i + 1];
-	else if (a[i + 1] == a[i + 2]) return a[i];
+	if (a[i] == a[i + 1])
+		return a[i + 2];
+	else if (a[i] == a[i + 2])
+		return a[i + 1];
+	else if (a[i + 1] == a[i + 2])
+		return a[i];
 }
